82849a8a8fa1fda9f2ec8046afa78a1973a6cd99
Vee Jay Grateja <grateja1001@gmail.com>
2022-07-22 08:07:57 +0800

    External view added with monitor checker

---
+++ public/checker/monitor.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+    header('Content-type: application/json');
+    $con = new PDO('mysql:host=localhost;dbname=csi_v3', 'csiv3', 'CSI_2020');
+
+    $stmt = $con->query('SELECT * FROM monitor_checkers');
+    $machine = $stmt->fetch();
+    echo(json_encode($machine));
+} catch (PDOException $ex) {
+    http_response_code(500);
+    echo "Something wrong" . $ex->getMessage();
+}
